3cf48b4de1fa0f5bcb9362a72f4af0c2a145e054
sharab786 <schou1@hotmail.com>
2021-11-29 15:28:10 +0000

    template for the contoller defined - feature-sup-14

--- src/main/java/com/qa/supermarket/contoller/ProductController.java
+++ src/main/java/com/qa/supermarket/contoller/ProductController.java
@@ -1,5 +1,24 @@
 package com.qa.supermarket.contoller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.supermarket.service.ProductService;
+
+//  Controllers Map to Requests
+@RestController     // This will make this class a rest controller
+@RequestMapping(path="/product")  // This designates a base path /product that all methods in the class will start with
 public class ProductController {
+	
+	private ProductService service;
+
+	// Constructor with the service as parameter to which it uses.
+	public ProductController(ProductService service) {
+		this.service = service;
+	} 
+	
+	// Mapping annotations below for different methods we would want to respond to the request.
+	
+	
 
 }
