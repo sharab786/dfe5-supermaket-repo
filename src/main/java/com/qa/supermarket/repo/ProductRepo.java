38d76cf1166c48415e6c177993bc1a58018fb3f9
sharab786 <schou1@hotmail.com>
2021-11-29 12:49:12 +0000

    Repo interface created for basics crud operations - feature-sup-12

--- src/main/java/com/qa/supermarket/repo/ProductRepo.java
+++ src/main/java/com/qa/supermarket/repo/ProductRepo.java
@@ -1,5 +1,15 @@
 package com.qa.supermarket.repo;
 
-public interface ProductRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.qa.supermarket.datamodel.Product;
+
+// Create an interface to the JpaRepsoitory 
+// (to the product class and the primary key is type Integer )
+// This will give us access to the basic CRUD operations
+// create, delete , insert and update.
+
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+	
+	
 }
